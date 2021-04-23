56f4b0fd3c60f832209303ae44d3f7dd88c27de6
bporto94 <bporto94@github.com.br>
2021-04-23 10:34:03 -0300

    Box Form

--- views/cadastro/portifolio.php
+++ views/cadastro/portifolio.php
@@ -198,7 +198,7 @@
             </div>
             <div class="clear"></div>
 
-        <div class="form">
+        <div class="row">
             <div class="row">
                 <h4>Informações Opcionais</h4>
             </div>
