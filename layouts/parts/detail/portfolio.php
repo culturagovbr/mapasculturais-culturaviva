b9721d9f65e1df4cc26ef06c6d513a497a072756
bporto94 <bporto94@github.com.br>
2021-04-29 16:22:31 -0300

    teste certificador

--- layouts/parts/detail/portfolio.php
+++ layouts/parts/detail/portfolio.php
@@ -1,46 +1,68 @@
 <div>
     <div class="form">
-<!--        <div class="row">-->
-<!--            <span><b> * Campos Obrigatórios </b></span>-->
-<!--        </div>-->
+        <!--        <div class="row">-->
+        <!--            <span><b> * Campos Obrigatórios </b></span>-->
+        <!--        </div>-->
         <!-- <h4>Informações Obrigatórias</h4> -->
         <div class="row">
             <span class="destaque">Portfólio*</span>
-                <div class="colunm1">
-                    <a ng-if="ponto['@files:portifolio'].url" href="{{ponto['@files:portifolio'].url}}" target="_blank">Baixar Arquivo</a>
-                    <a style="font-size: 12px" ng-if="ponto.atividadesEmRealizacaoLink" href="{{ponto.atividadesEmRealizacaoLink}}" target="_blank">{{ponto.atividadesEmRealizacaoLink}}</a>
-                    <span ng-if="!ponto['@files:portifolio'].url && !ponto.atividadesEmRealizacaoLink"><b>Não informado</b></span>
-                </div>
+            <div class="colunm1">
+                <a ng-if="ponto['@files:portifolio'].url" href="{{ponto['@files:portifolio'].url}}" target="_blank">
+                    <img ng-if="agent.files.portifolio" src="<?php $this->asset('img/pdflogo.png') ?>" width="160"
+                         height="138">
+                    Baixar Arquivo
+                </a>
+                <a style="font-size: 12px" ng-if="ponto.atividadesEmRealizacaoLink"
+                   href="{{ponto.atividadesEmRealizacaoLink}}" target="_blank">{{ponto.atividadesEmRealizacaoLink}}</a>
+                <span ng-if="!ponto['@files:portifolio'].url && !ponto.atividadesEmRealizacaoLink"><b>Não informado</b></span>
+            </div>
         </div>
         <div ng-if="entidade.tipoOrganizacao == 'coletivo'">
-          <h4>Ata de composição e constituição do coletivo</h4>
-          <div class="row">
-              <div class="colunm1">
-                    <a ng-if="ponto['@files:ata'].url" href="{{ponto['@files:ata'].url}}" target="_blank">Baixar Arquivo</a>
+            <h4>Ata de composição e constituição do coletivo</h4>
+            <div class="row">
+                <div class="colunm1">
+                    <a ng-if="ponto['@files:ata'].url" href="{{ponto['@files:ata'].url}}" target="_blank">
+                        <img ng-if="agent.files.ata" src="<?php $this->asset('img/pdflogo.png') ?>" width="160"
+                             height="138">
+                        Baixar Arquivo
+                    </a>
                     <span ng-if="!ponto['@files:ata'].url"><b>Não informado</b></span>
-              </div>
-          </div>
+                </div>
+            </div>
         </div>
 
         <!-- <div class="row"> -->
-            <!-- <h4>Cartas de Reconhecimento</h4> -->
+        <!-- <h4>Cartas de Reconhecimento</h4> -->
         <!-- </div> -->
         <div class="row">
             <span class="destaque">Cartas de Reconhecimento*</span>
             <div class="colunm1">
-              <a style="font-size: 12px; color: inherit" ng-if="ponto['@files:carta1'].url" href="{{ponto['@files:carta1'].url}}" target="_blank">{{agent.files.carta1.name}}Baixar primeira carta</a>
-              <span ng-if="!ponto['@files:carta1'].url"><b>Não informado</b></span>
+                <img ng-if="agent.files.carta1" src="<?php $this->asset('img/pdflogo.png') ?>" width="160" height="138">
+                <a style="font-size: 12px; color: inherit" ng-if="ponto['@files:carta1'].url"
+                   href="{{ponto['@files:carta1'].url}}" target="_blank">
+                    <img ng-if="agent.files.carta1" src="<?php $this->asset('img/pdflogo.png') ?>" width="160"
+                         height="138">
+                    {{agent.files.carta1.name}}
+                    Baixar primeira carta</a>
+                <span ng-if="!ponto['@files:carta1'].url"><b>Não informado</b></span>
             </div>
             <div class="colunm2">
-              <a style="font-size: 12px; color: inherit" ng-if="ponto['@files:carta2'].url" href="{{ponto['@files:carta2'].url}}" target="_blank">{{agent.files.carta2.name}}Baixar segunda carta</a>
-              <span ng-if="!ponto['@files:carta2'].url"><b>Não informado</b></span>
+                <a style="font-size: 12px; color: inherit" ng-if="ponto['@files:carta2'].url"
+                   href="{{ponto['@files:carta2'].url}}" target="_blank">
+                    <img ng-if="agent.files.carta2" src="<?php $this->asset('img/pdflogo.png') ?>" width="160"
+                         height="138">
+                    {{agent.files.carta2.name}}
+                    Baixar segunda carta
+                </a>
+                <span ng-if="!ponto['@files:carta2'].url"><b>Não informado</b></span>
             </div>
         </div>
         <div class="clear"></div>
     </div>
     <div class="form form-opcional">
         <div class="row">
-            <span class="destaque">Conte um pouco sobre a história do ponto de Cultura (800 caracteres) <i class='hltip' title='Nos diga um pouco mais sobre o ponto de cultura, como por exemplo como ele começou e como surgiu a idéia'>?</i>  </span>
+            <span class="destaque">Conte um pouco sobre a história do ponto de Cultura (800 caracteres) <i class='hltip'
+                                                                                                           title='Nos diga um pouco mais sobre o ponto de cultura, como por exemplo como ele começou e como surgiu a idéia'>?</i>  </span>
             <label class="colunm1">
                 <span><b>{{ponto.longDescription}}</b></span>
                 <span ng-if="!ponto.longDescription"><b>Não informado</b></span>
