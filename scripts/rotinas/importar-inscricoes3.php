fa2fb820184922465bf3c616334ea65417900277
bporto94 <bporto94@github.com.br>
2021-04-30 18:11:27 -0300

    debbugin redistribuicao

--- scripts/rotinas/importar-inscricoes3.php
+++ scripts/rotinas/importar-inscricoes3.php
@@ -170,7 +170,7 @@ function importar()
     $ufs = $conn->fetchAll("SELECT * FROM culturaviva.uf;");
 
     foreach ($ufs as $uf) {
-        if ($uf['sigla'] == 'AC') {
+        if ($uf['sigla'] == $uf) {
             print("Distribuir avaliações para Representantes da Sociedade Civil Estadual\n");
             inserirAvaliacaoCertificador($conn, ['tipo' => 'S'], $uf);
 
