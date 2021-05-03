d2dfc2699481e2e3af82c624c61c1a27878a8c1f
bporto94 <bporto94@github.com.br>
2021-05-03 14:59:55 -0300

    corrigindo config.php

--- config.php
+++ config.php
@@ -8,14 +8,14 @@ return [
     'auth.config' => array(
         'client_id' => '3_rzes4uxsgv4k4o8scscg8w40ks08c08sgg4488o4wskcko4wk',
         'client_secret' => '2k0i4p6zzvac0woww00g0o0ockgc84k04ogwo800sw4w8cwcgs',
-        'auth_endpoint' => 'https://dev-id.cultura.gov.br/oauth/v2/auth',
-        'token_endpoint' => 'https://dev-id.cultura.gov.br/oauth/v2/token',
+        'auth_endpoint' => 'https://hmg-id.cultura.gov.br/oauth/v2/auth',
+        'token_endpoint' => 'https://hmg-id.cultura.gov.br/oauth/v2/token',
         /*
         Nova ?
         'auth_endpoint' => 'https://id.cultura.gov.br/openid/connect/authorize',
         'token_endpoint' => 'https://id.cultura.gov.br/openid/connect/token',
         */
-        'user_info_endpoint' => 'https://dev-id.cultura.gov.br/api/v1/person.json',
-        'onCreateRedirectUrl' => 'http://dev.culturaviva.gov.br/rede/entrada'
+        'user_info_endpoint' => 'https://hmg-id.cultura.gov.br/api/v1/person.json',
+        'onCreateRedirectUrl' => 'http://hmg.culturaviva.gov.br/rede/entrada'
     )
 ];
