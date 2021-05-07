5ea3447db4458cf983491ffe7f50501b3426ae08
bporto94 <bporto94@github.com.br>
2021-05-07 15:52:40 -0300

    verificação de campos

--- assets/js/cadastro-controller.js
+++ assets/js/cadastro-controller.js
@@ -785,6 +785,11 @@
                 if ($location.search().invalid === '1') {
                     $scope.showInvalid($scope.agent.rcv_tipo, 'form_portifolio');
                 }
+
+                if ((sucesso.longDescription.indexOf("3") > 1) || (sucesso.longDescription > 1)) {
+                    $scope.save_field('longDescription');
+                    $scope.messages.show('sucesso', 'alterações salvas');
+                }
             });
 
 
