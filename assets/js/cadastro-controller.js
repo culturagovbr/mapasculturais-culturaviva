04cf8f00d5d0206aa72a5c7de3c15c38fefb2217
bporto94 <bporto94@github.com.br>
2021-05-25 15:31:34 -0300

    Portifolio

--- assets/js/cadastro-controller.js
+++ assets/js/cadastro-controller.js
@@ -497,12 +497,12 @@
                     if ($scope.data.statusInscricao == 1) {
                         ngDialog.open({
                             template: 'modal1',
-                            scope: $scope
+                            scope: $scope.data
                         });
                     } else {
                         ngDialog.open({
                             template: 'modal2',
-                            scope: $scope
+                            scope: $scope.data
                         });
                     }
                     $scope.saveObs = function () {
