150d8af246465bd063ae6308e9eddc5a2a844f76
bporto94 <bporto94@github.com.br>
2021-05-25 11:33:46 -0300

    Portifolio

--- Controllers/Cadastro.php
+++ Controllers/Cadastro.php
@@ -102,7 +102,6 @@ class Cadastro extends \MapasCulturais\Controller{
         $required_properties = [
             'name',
             'shortDescription',
-            'longDescription',
             'tem_sede',
             'pais',
             'location', // ponto no mapa
