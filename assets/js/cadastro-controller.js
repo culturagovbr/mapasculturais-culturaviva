660e92b0e50b15b763579af2703e7fbea70dd53d
pablo.goncalves <pablorgds@gmail.com>
2021-06-07 10:57:09 -0300

    Ajuste campo "longdescription" aba portifolio (UPDATE)

--- assets/js/cadastro-controller.js
+++ assets/js/cadastro-controller.js
@@ -761,7 +761,7 @@
 
             var params = {
                 'id': agent_id,
-                '@select': 'id,rcv_tipo,atividadesEmRealizacao,atividadesEmRealizacaoLink,longDescription,',
+                '@select': 'id,rcv_tipo,atividadesEmRealizacao,longDescription,',
                 '@files': '(portifolio,gallery,carta1,carta2,ata):url',
                 '@permissions': 'view'
             };
