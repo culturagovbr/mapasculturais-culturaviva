66f94bb19dd36abdbcd6834280471dc0c2dedd0d
pablo.goncalves <pablorgds@gmail.com>
2021-06-09 14:38:00 -0300

    Visualizar certificado

--- assets/admin/src/modules/Certificacao/controllers/AvaliacaoCertificadoCtrl.js
+++ assets/admin/src/modules/Certificacao/controllers/AvaliacaoCertificadoCtrl.js
@@ -117,66 +117,62 @@ function AvaliacaoCertificadoCtrl($scope, $state, $http, $window) {
     }
 
     function createPDF() {
-        var doc = new jsPDF()
+        // var doc = new jsPDF()
+        //
+        // doc.text('Hello world!', 10, 10)
+        // doc.save('a4.pdf')
+        var qr = document.getElementById('qrcode');
 
-        doc.text('Hello world!', 10, 10)
-        doc.save('a4.pdf')
+        function convertImgToBase64(callback) {
+            var img = new Image();
+            img.onload = function () {
+                var canvas = document.createElement('CANVAS');
+                var ctx = canvas.getContext('2d');
+                // canvas.height = 1241;
+                // canvas.width = 1754;
+                canvas.height = img.height;
+                canvas.width = img.width;
+                ctx.drawImage(this, 0, 0);
+                var dataURL = canvas.toDataURL('image/jpeg');
+                if (typeof callback === 'function') {
+                    callback(dataURL);
+                }
+                // canvas = null;
+            };
+            img.src = '/assets/img/certificado.png';
+        }
 
+        var button = document.getElementById("download");
 
-        // $scope.createPdf = function () {
-        //     var qr = document.getElementById('qrcode');
-        //
-        //     function convertImgToBase64(callback) {
-        //         var img = new Image();
-        //         img.onload = function () {
-        //             var canvas = document.createElement('CANVAS');
-        //             var ctx = canvas.getContext('2d');
-        //             // canvas.height = 1241;
-        //             // canvas.width = 1754;
-        //             canvas.height = img.height;
-        //             canvas.width = img.width;
-        //             ctx.drawImage(this, 0, 0);
-        //             var dataURL = canvas.toDataURL('image/jpeg');
-        //             if (typeof callback === 'function') {
-        //                 callback(dataURL);
-        //             }
-        //             // canvas = null;
-        //         };
-        //         img.src = '/assets/img/certificado.png';
-        //     }
-        //
-        //     var button = document.getElementById("download");
-        //
-        //     convertImgToBase64(function (dataUrl) {
-        //         var doc = new jsPDF('l', 'pt', [1755, 1238]);
-        //
-        //         doc.addImage(dataUrl, 'png', 0, 0, 1755, 1238, '', 'NONE');
-        //
-        //         doc.setFontType("bold");
-        //         doc.setTextColor("#FFFFFF");
-        //         doc.setFontSize(35);
-        //         var text = "A Secretaria Especial da Cultura do Ministério da Cidadania, por meio da Secretaria da Diversidade Cultural, reconhece o coletivo/entidade\n\n" +
-        //             "\n\n" +
-        //             "como Ponto de Cultura a partir dos critérios estabelecidos na Lei Cultura Viva (13.018/2014).\n\n" +
-        //             "Este certificado comprova que a iniciativa desenvolve e articula atividades culturais em sua comunidade, " +
-        //             "e contribui para o acesso, a proteção e a promoção dos direitos, da cidadania e da diversidade cultural no Brasil."
-        //
-        //         var text = doc.splitTextToSize(text, 1090)
-        //         doc.text(text, 490, 290, '', '', 'center');
-        //
-        //         var name = doc.splitTextToSize($scope.ponto.name, 1400)
-        //         doc.setFontSize(25);
-        //         doc.text(name, 490, 410);
-        //
-        //         var dataURLQR = qr.children[0].toDataURL('image/png');
-        //         doc.setFontSize(20);
-        //         doc.text(MapasCulturais.createUrl('agent', 'single', [ponto.id]), 630, 1225);
-        //         doc.addImage(dataURLQR, 'png', 659, 996, 200, 199);
-        //
-        //         doc.save('Certificado.pdf');
-        //         return doc;
-        //     });
-        // }
+        convertImgToBase64(function (dataUrl) {
+            var doc = new jsPDF('l', 'pt', [1755, 1238]);
+
+            doc.addImage(dataUrl, 'png', 0, 0, 1755, 1238, '', 'NONE');
+
+            doc.setFontType("bold");
+            doc.setTextColor("#FFFFFF");
+            doc.setFontSize(35);
+            var text = "A Secretaria Especial da Cultura do Ministério da Cidadania, por meio da Secretaria da Diversidade Cultural, reconhece o coletivo/entidade\n\n" +
+                "\n\n" +
+                "como Ponto de Cultura a partir dos critérios estabelecidos na Lei Cultura Viva (13.018/2014).\n\n" +
+                "Este certificado comprova que a iniciativa desenvolve e articula atividades culturais em sua comunidade, " +
+                "e contribui para o acesso, a proteção e a promoção dos direitos, da cidadania e da diversidade cultural no Brasil."
+
+            var text = doc.splitTextToSize(text, 1090)
+            doc.text(text, 490, 290, '', '', 'center');
+
+            var name = doc.splitTextToSize($scope.ponto.name, 1400)
+            doc.setFontSize(25);
+            doc.text(name, 490, 410);
+
+            var dataURLQR = qr.children[0].toDataURL('image/png');
+            doc.setFontSize(20);
+            doc.text(MapasCulturais.createUrl('agent', 'single', [ponto.id]), 630, 1225);
+            doc.addImage(dataURLQR, 'png', 659, 996, 200, 199);
+
+            doc.save('Certificado.pdf');
+            return doc;
+        });
     }
 }
 
