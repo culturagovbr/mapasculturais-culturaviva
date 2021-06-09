6ba119ea76109d8fb8e653407464ae120b1337cf
pablo.goncalves <pablorgds@gmail.com>
2021-06-09 14:09:07 -0300

    Visualizar certificado

--- assets/admin/src/modules/Certificacao/controllers/AvaliacaoCertificadoCtrl.js
+++ assets/admin/src/modules/Certificacao/controllers/AvaliacaoCertificadoCtrl.js
@@ -117,7 +117,6 @@ function AvaliacaoCertificadoCtrl($scope, $state, $http, $window) {
     }
 
     function createPDF() {
-        console.log('aqui')
         $scope.createPdf = function () {
             var qr = document.getElementById('qrcode');
 
