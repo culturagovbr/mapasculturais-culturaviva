9260a967a4ba78bc1f9c9496936d53ad66caff5e
pablo.goncalves <pablorgds@gmail.com>
2021-06-09 15:24:54 -0300

    Visualizar certificado

--- assets/admin/src/modules/Certificacao/Certificacao.js
+++ assets/admin/src/modules/Certificacao/Certificacao.js
@@ -2,12 +2,3 @@
 
 angular
     .module('Certificacao', []);
-
-var app = angular.module('culturaviva.services', ['ngResource']);
-
-app.factory('MapasCulturais', function () {
-    if (!window.MapasCulturais) {
-        throw new Error('É necessário ter o obj "MapasCulturais" em window');
-    }
-    return window.MapasCulturais;
-});
