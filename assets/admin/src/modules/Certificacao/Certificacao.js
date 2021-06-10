4eaaf01d120b896c71fbb24ce0f9fe4bff281ff4
pablo.goncalves <pablorgds@gmail.com>
2021-06-10 09:06:16 -0300

    Visualizar certificado

--- assets/admin/src/modules/Certificacao/Certificacao.js
+++ assets/admin/src/modules/Certificacao/Certificacao.js
@@ -1,11 +1,4 @@
 'use strict';
 
 angular
-    .module('Certificacao', ['ngResource']);
-
-app.factory('MapasCulturais', function () {
-    if (!window.MapasCulturais) {
-        throw new Error('É necessário ter o obj "MapasCulturais" em window');
-    }
-    return window.MapasCulturais;
-});
+    .module('Certificacao', ['']);
