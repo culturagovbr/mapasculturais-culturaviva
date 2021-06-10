795e607590740bdaf4556fc56c3c260e0a6e191d
pablo.goncalves <pablorgds@gmail.com>
2021-06-10 10:14:04 -0300

    Visualizar certificado

--- assets/admin/src/modules/Certificacao/Certificacao.js
+++ assets/admin/src/modules/Certificacao/Certificacao.js
@@ -1,4 +1,13 @@
-'use strict';
+(function (angular) {
+    'use strict';
 
-angular
-    .module('Certificacao', ['']);
+    var app = angular.module('Certificacao', ['ngResource']);
+
+    app.factory('MapasCulturais', function () {
+        if (!window.MapasCulturais) {
+            throw new Error('É necessário ter o obj "MapasCulturais" em window');
+        }
+        return window.MapasCulturais;
+    });
+
+})(angular);
