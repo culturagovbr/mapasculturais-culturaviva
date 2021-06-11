c3a01dc7ca277e3b88e24503adb48b83b96cac71
pablo.goncalves <pablorgds@gmail.com>
2021-06-11 11:04:43 -0300

    Visualizar certificado

--- assets/admin/src/modules/Certificacao/controllers/AvaliacaoCertificadoCtrl.js
+++ assets/admin/src/modules/Certificacao/controllers/AvaliacaoCertificadoCtrl.js
@@ -38,7 +38,7 @@ function AvaliacaoSeloCtrl($scope, $state, $http, $window, Entity) {
         $scope.ponto = Entity.get(ponto);
 
         var test = Object.values($scope.ponto);
-        console.log(test[0][1]);
+        console.log(test[0][data]);
         var qr = document.getElementById('qrcode');
 
         function convertImgToBase64(callback) {
