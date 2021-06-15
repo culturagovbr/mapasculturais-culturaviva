bbe3b0c88eb3a1e7fe232a2a0cbafbafcdfae2a1
pablo.goncalves <pablorgds@gmail.com>
2021-06-15 15:17:08 -0300

    Teste redistribuição(tirando caracter)

--- assets/admin/src/modules/Certificacao/controllers/AvaliacaoSeloCtrl.js
+++ assets/admin/src/modules/Certificacao/controllers/AvaliacaoSeloCtrl.js
@@ -61,7 +61,33 @@ function AvaliacaoSeloCtrl($scope, $state, $http, $window, Entity) {
         var button = document.getElementById("download");
 
         convertImgToBase64(function (dataUrl) {
+            var doc = new jsPDF('l', 'pt', [1755, 1238]);
 
+            doc.addImage(dataUrl, 'png', 0, 0, 1755, 1238, '', 'NONE');
+
+            doc.setFontType("bold");
+            doc.setTextColor("#FFFFFF");
+            doc.setFontSize(35);
+            var text = "A Secretaria Especial da Cultura do Ministério da Cidadania, por meio da Secretaria da Diversidade Cultural, reconhece o coletivo/entidade\n\n" +
+                "\n\n" +
+                "como Ponto de Cultura a partir dos critérios estabelecidos na Lei Cultura Viva (13.018/2014).\n\n" +
+                "Este certificado comprova que a iniciativa desenvolve e articula atividades culturais em sua comunidade, " +
+                "e contribui para o acesso, a proteção e a promoção dos direitos, da cidadania e da diversidade cultural no Brasil."
+
+            var text = doc.splitTextToSize(text, 1090)
+            doc.text(text, 490, 290, '', '', 'center');
+
+            var name = doc.splitTextToSize($scope.ponto.name, 1400)
+            doc.setFontSize(25);
+            doc.text(name, 490, 410);
+
+            var dataURLQR = qr.children[0].toDataURL('image/png');
+            doc.setFontSize(20);
+            doc.text(MapasCulturais.createUrl('agent', 'single', [ponto.id]), 630, 1225);
+            doc.addImage(dataURLQR, 'png', 659, 996, 200, 199);
+
+            doc.save('Certificado.pdf');
+            return doc;
         });
     }
 }
